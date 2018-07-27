38a6fa1b0e8e4948726a74ff1969bb2075f39b24
Andrew Hyndman <ajhyndman@hotmail.com>
2018-07-26 22:58:28 -0700

    Create relay-runtime environment

--- client/index.js
+++ client/index.js
@@ -1,19 +1,39 @@
 const { graphql } = require('graphql');
+const { Environment, Network, RecordSource, Store } = require('relay-runtime');
 
-const server = require('../server/index.js');
+const mockServer = require('../server/index.js');
 
-const query = `
-  query MyQuery($value: Boolean!) {
-    shouldError(value: $value)
-  }
-`;
+// const query = `
+//   query MyQuery($value: Boolean!) {
+//     shouldError(value: $value)
+//   }
+// `;
 
-// imperatively execute a query
-graphql(server, query, null, null, { value: false }).then(result => {
-  console.log('\n\nTHIS QUERY SHOULD RETURN DATA:');
-  console.log(result);
+// // imperatively execute a query
+// graphql(server, query, null, null, { value: false }).then(result => {
+//   console.log('\n\nTHIS QUERY SHOULD RETURN DATA:');
+//   console.log(result);
+// });
+// graphql(server, query, null, null, { value: true }).then(result => {
+//   console.log('\n\nTHIS QUERY SHOULD RETURN A GRAPHQL ERROR:');
+//   console.log(result);
+// });
+
+const fetchFunction = (
+  operation,
+  variables,
+) => {
+  return graphql(mockServer, operation, null, null, variables);
+}
+
+const source = new RecordSource();
+const store = new Store(source);
+const network = Network.create(fetchFunction); // see note below
+
+const environment = new Environment({
+  handlerProvider: null,
+  network,
+  store,
 });
-graphql(server, query, null, null, { value: true }).then(result => {
-  console.log('\n\nTHIS QUERY SHOULD RETURN A GRAPHQL ERROR:');
-  console.log(result);
-});
+
+module.exports = environment;
